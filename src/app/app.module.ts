fcef0024e2914d0f7c68ff514c9012f0fe1a31ff
jorge <jrrdl1506mx@gmail.com>
2023-06-06 13:30:04 -0600

    Tabla comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { TablaCoordComponent } from './tabla-coord/tabla-coord.component';
-=======
 import { MapPageComponent } from './map-page/map-page.component';
->>>>>>> 714d8fb6f2472a6c39cbe0d23a6439ba7aedce33
+import { TablaCoordComponent } from './tabla-coord/tabla-coord.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
+    MapPageComponent,
     TablaCoordComponent
-=======
-    MapPageComponent
->>>>>>> 714d8fb6f2472a6c39cbe0d23a6439ba7aedce33
   ],
   imports: [
     BrowserModule,
