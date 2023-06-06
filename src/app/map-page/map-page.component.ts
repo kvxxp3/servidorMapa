e30c7a0dea95930b720b9743eede462364f4f647
kvxxp3 <quezadakarla2406@gmail.com>
2023-06-06 14:24:53 -0600

    Reinicia LocalStorage

--- src/app/map-page/map-page.component.ts
+++ src/app/map-page/map-page.component.ts
@@ -14,6 +14,7 @@ export class MapPageComponent{
 
   constructor(private _route:ActivatedRoute, private _router: Router){
     this._route.params.subscribe((params: Params) => {
+      //recibe los datos de los parametros de la ruta definidos en app-routing.module.ts
       this.altitud=params['altitud'];
       this.latitud=params['latitud'];      
       this.hora=params['hora'];
@@ -25,12 +26,16 @@ export class MapPageComponent{
   }
 
   guardarDatos(){
+    //elimina datos previos del local storage
+    localStorage.clear();
+    //guarda los nuevos datos
     localStorage.setItem('altitud', JSON.stringify(this.altitud));
     localStorage.setItem('latitud', JSON.stringify(this.latitud));
     localStorage.setItem('hora', JSON.stringify(this.hora));
   }
 
   recuperarDatos(){
+    //guarda todos los datos en una misma variable
     this.datos = "Altitud: " + JSON.parse(localStorage.getItem('altitud') || '{}');
     this.datos += " Latitud: " + JSON.parse(localStorage.getItem('latitud') || '{}');
     this.datos += " Hora: " + JSON.parse(localStorage.getItem('hora') || '{}');
